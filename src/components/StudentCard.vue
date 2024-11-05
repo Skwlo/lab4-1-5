c5bf53fa348f9fc4cdb97ebd3f6b42aefd2bbdd1
Skwlo <130392852+Skwlo@users.noreply.github.com>
2024-11-05 22:11:51 +0800

    Add files via upload

---
+++ src/components/StudentCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { StudentEvent } from '@/type'
+defineProps<{
+  event: StudentEvent
+}>()
+</script>
+
+<template>
+<div class="event-class">
+    <div class="event-studentcard">
+      <h2>{{ event.name }}</h2>
+      <span>@{{ event.surname }} on {{ event.gpa }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.event-studentcard {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-studentcard:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
